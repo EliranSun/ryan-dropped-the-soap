7202c34e8728e40b9093899c64eca4576f94ada5
Eliran Shemesh <piro27@gmail.com>
2024-05-13 00:47:55 +0300

    Water level adjustments

--- Assets/Scripts/WaterLevel.cs
+++ Assets/Scripts/WaterLevel.cs
@@ -1,16 +1,34 @@
+using System;
 using UnityEngine;
 
 public class WaterLevel : MonoBehaviour {
-    private int _waterThrottle;
+    [SerializeField] private float waterThrottle;
 
     private void Update() {
-        if (_waterThrottle == 0) return;
+        if (waterThrottle == 0)
+            return;
 
-        transform.Translate(new Vector2(0, (float)1 / _waterThrottle * Time.deltaTime));
+        transform.Translate(new Vector2(0, waterThrottle * Time.deltaTime));
     }
 
     public void OnNotify(GameEvents eventName) {
-        if (eventName == GameEvents.FaucetOpening) _waterThrottle -= 10;
-        if (eventName == GameEvents.FaucetOpening) _waterThrottle += 10;
+        switch (eventName) {
+            case GameEvents.FaucetOpening:
+                waterThrottle += 0.05f;
+                break;
+
+            case GameEvents.FaucetClosing:
+                if (waterThrottle <= 0)
+                    return;
+
+                waterThrottle -= 0.05f;
+                break;
+
+            case GameEvents.None:
+                break;
+
+            default:
+                throw new ArgumentOutOfRangeException(nameof(eventName), eventName, null);
+        }
     }
 }
