5703757d4a9afb39f6536d73b479d3d15d144e3b
Eliran Shemesh <piro27@gmail.com>
2024-08-04 12:07:27 +0300

    Going to a floor logic - wip

--- Assets/Scripts/Character/Scripts/Movement.cs
+++ Assets/Scripts/Character/Scripts/Movement.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+namespace Character.Scripts
 {
-    // Start is called before the first frame update
-    void Start()
+    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Movement : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private float speed = 5f;
+        private Rigidbody2D _rigidbody2D;
+        private SpriteRenderer _spriteRenderer;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Start()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void Update()
+        {
+            var horizontal = Input.GetAxis("Horizontal");
+            var vertical = Input.GetAxis("Vertical");
+            _rigidbody2D.velocity = new Vector2(horizontal * speed, vertical * speed);
+
+            if (horizontal > 0)
+                _spriteRenderer.flipX = false;
+            else if (horizontal < 0)
+                _spriteRenderer.flipX = true;
+        }
     }
 }
--- Assets/Scripts/Elevator/scripts/ElevatorController.cs
+++ Assets/Scripts/Elevator/scripts/ElevatorController.cs
@@ -1,15 +1,65 @@
-using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Elevator.scripts
 {
     public class ElevatorController : MonoBehaviour
     {
         [SerializeField] private GameObject panel;
+        [SerializeField] private TextMeshPro floorText;
+        [SerializeField] private TextMeshPro currentFloorText;
+        [SerializeField] private float debounce = 3f;
+        private int _currentFloor;
+        private float _timeDiff;
+        private float _timeSinceLastClick;
 
         private void Start()
         {
-            panel.SetActive(false);
+            var panelChildren = panel.transform.childCount;
+            for (var i = 0; i < panelChildren; i++)
+            {
+                var child = panel.transform.GetChild(i);
+                var button = child.GetComponent<Button>();
+                button.onClick.AddListener(() => OnElevatorButtonClick(button));
+            }
+        }
+
+        private void Update()
+        {
+            _timeDiff += Time.deltaTime;
+            _timeSinceLastClick += Time.deltaTime;
+
+            if (floorText.text == "0")
+                return;
+
+            if (_currentFloor != int.Parse(floorText.text))
+                return;
+
+            if (!(_timeSinceLastClick > debounce))
+                return;
+
+            var floor = int.Parse(floorText.text);
+            GoToFloor(floor);
+            floorText.text = "0";
+        }
+
+        private void OnElevatorButtonClick(Button button)
+        {
+            _timeSinceLastClick = 0;
+
+            var buttonName = button.name;
+            var floor = int.Parse(buttonName);
+            floorText.text = floorText.text == "0" // init state
+                ? $"{floor}"
+                : $"{floorText.text}{floor}";
+        }
+
+        private void GoToFloor(int floorNumber)
+        {
+            _timeSinceLastClick = 0;
+            print($"Going to floor {floorNumber}");
+            currentFloorText.text = $"{floorNumber}";
         }
     }
 }
