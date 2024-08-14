e5c670acba47ecec67a9f4da4a95c4dd3ea4cc6e
Eliran Shemesh <piro27@gmail.com>
2024-08-14 21:40:22 +0300

    wip

---
+++ Assets/Scripts/Character Creator/scripts/CharacterCreator.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Character_Creator.scripts
+{
+    public class CharacterCreator : MonoBehaviour
+    {
+        [SerializeField] private Sprite[] face;
+        [SerializeField] private Sprite[] eyes;
+
+        private void Start()
+        {
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+        }
+    }
+}
