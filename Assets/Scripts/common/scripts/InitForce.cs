3e315adeead36a1246eaf31c380ef4803f2c8c91
Eliran Shemesh <piro27@gmail.com>
2025-02-08 16:54:45 +0200

    The beach scene start

---
+++ Assets/Scripts/common/scripts/InitForce.cs
@@ -0,0 +1,53 @@
+using System;
+using UnityEngine;
+
+namespace common.scripts
+{
+    [Serializable]
+    internal enum Force
+    {
+        Soft,
+        Medium,
+        Hard
+    }
+
+
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class InitForce : MonoBehaviour
+
+    {
+        [SerializeField] private float delayForce;
+        [SerializeField] private Force force;
+        [SerializeField] private Direction direction;
+        private Vector3 _directionValue;
+        private float _forceValue;
+        private Rigidbody2D _rigidbody;
+
+
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+            _forceValue = force switch
+            {
+                Force.Soft => 1f,
+                Force.Medium => 3f,
+                _ => 3f
+            };
+            _directionValue = direction switch
+            {
+                Direction.Up => Vector3.up,
+
+                Direction.Down => Vector3.down,
+                Direction.Left => Vector3.left,
+                _ => Vector3.right
+            };
+
+            Invoke(nameof(AddForce), delayForce);
+        }
+
+        private void AddForce()
+        {
+            _rigidbody.AddForce(_directionValue * _forceValue, ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/common/scripts/ObjectsAutoMove.cs
@@ -0,0 +1,52 @@
+using System;
+using UnityEngine;
+
+namespace common.scripts
+{
+    [Serializable]
+    internal enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    [Serializable]
+    internal enum Speed
+    {
+        Slow,
+        Medium,
+        Fast
+    }
+
+    public class ObjectsAutoMove : MonoBehaviour
+
+    {
+        [SerializeField] private Direction direction;
+        [SerializeField] private Speed speed;
+
+
+        private void Update()
+        {
+            var speed = this.speed switch
+            {
+                Speed.Slow => 0.05f,
+                Speed.Medium => 0.1f,
+                Speed.Fast => 0.15f,
+                _ => 0.1f
+            };
+
+            var direction = this.direction switch
+            {
+                Direction.Up => Vector3.up,
+                Direction.Down => Vector3.down,
+                Direction.Left => Vector3.left,
+                Direction.Right => Vector3.right,
+                _ => Vector3.right
+            };
+
+            transform.Translate(direction * (speed * Time.deltaTime));
+        }
+    }
+}
