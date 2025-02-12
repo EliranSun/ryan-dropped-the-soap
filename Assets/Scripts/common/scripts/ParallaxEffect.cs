9a48c40f8236c87e7305aa9ffbc97bd95e8d6714
Eliran Shemesh <piro27@gmail.com>
2025-02-12 09:35:56 +0200

    Boat & Sea

--- Assets/Scripts/common/scripts/ParallaxEffect.cs
+++ Assets/Scripts/common/scripts/ParallaxEffect.cs
@@ -11,9 +11,10 @@ namespace common.scripts
             "Set this between 0 and 1 for background layers (e.g., 0.5). Use values greater than 1 for foreground layers.")]
         public float parallaxMultiplier = 0.5f;
 
+        public float yOffset;
         public bool disableYAxis;
-        private float _originalY;
         private Vector3 _lastCameraPosition;
+        private float _originalY;
 
 
         private void Start()
@@ -32,6 +33,7 @@ namespace common.scripts
             // Move the background or foreground object
             transform.position += deltaMovement * parallaxMultiplier;
             if (disableYAxis) transform.position = new Vector3(transform.position.x, _originalY, transform.position.z);
+            if (yOffset != 0) transform.position = new Vector3(transform.position.x, _originalY + yOffset, transform.position.z);
 
             // Update the last camera position
             _lastCameraPosition = cameraTransform.position;
