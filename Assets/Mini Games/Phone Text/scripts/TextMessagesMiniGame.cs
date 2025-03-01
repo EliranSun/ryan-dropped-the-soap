ea2beff75651f7ef492f3d6e2e8922b7715aea4f
Eliran Shemesh <piro27@gmail.com>
2025-03-01 11:43:26 +0200

    Zeke: Implement text messages mini-game with dynamic message generation and interaction

--- Assets/Mini Games/Phone Text/scripts/TextMessagesMiniGame.cs
+++ Assets/Mini Games/Phone Text/scripts/TextMessagesMiniGame.cs
@@ -1,11 +1,21 @@
+using System;
+using Dialog.Scripts;
 using UnityEngine;
 
 namespace Mini_Games.Phone_Text.scripts
 {
+    [Serializable]
+    public class WifeMessage
+    {
+        public string text;
+    }
+
     public class PhoneTextMiniGame : MonoBehaviour
     {
         [SerializeField] private GameObject textMessage;
         [SerializeField] private GameObject miniGameContainer;
+        [SerializeField] private WifeMessage[] wifeMessages;
+        [SerializeField] private PlayerMiniGameChoice[] choices;
 
 
         private void Start()
