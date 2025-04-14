df3052580b9f56323c35105a11b604ce0ba47215
Eliran Shemesh <piro27@gmail.com>
2025-04-14 15:53:50 +0300

    Stacy: Start dialog sequence

---
+++ Assets/Scenes/Stacy/scripts/InventorySystem.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySystem : MonoBehaviour
+{
+    [SerializeField] Image flashlightImage;
+
+    private void Start()
+    {
+        //flashlightImage;
+    }
+}
