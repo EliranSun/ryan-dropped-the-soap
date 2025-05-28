483f8dd66572d1db24bd0b6627bed3c05859d355
Eliran Shemesh <piro27@gmail.com>
2025-05-28 10:10:26 +0300

    CSS-98: Fix dialog boxes & connect zeke dialog

--- Assets/Scripts/DraggableObject.cs
+++ Assets/Scripts/DraggableObject.cs
@@ -5,6 +5,7 @@ public class DraggableObject : ObserverSubject
     [SerializeField] private GameEvents onDragEvent;
     [SerializeField] private GameEvents onDropEvent;
     [SerializeField] private bool _is3D = false;
+    [SerializeField] private LayerMask draggableLayerMask = -1; // Which layers can be dragged
 
     private bool _isDragging;
     private bool _isSticky;
@@ -18,6 +19,10 @@ public class DraggableObject : ObserverSubject
     private Plane _dragPlane;
     private Vector3 _dragOffset;
 
+    // Manual input handling
+    private Collider _collider3D;
+    private Collider2D _collider2D;
+
     private void Start()
     {
         _mainCamera = Camera.main;
@@ -25,13 +30,15 @@ public class DraggableObject : ObserverSubject
         if (_is3D)
         {
             _rigidbody3D = GetComponent<Rigidbody>();
+            _collider3D = GetComponent<Collider>();
+
             if (_rigidbody3D == null)
             {
                 Debug.LogError("DraggableObject: 3D mode requires a Rigidbody component");
                 enabled = false;
             }
 
-            if (GetComponent<Collider>() == null)
+            if (_collider3D == null)
             {
                 Debug.LogError("DraggableObject: 3D mode requires a Collider component");
                 enabled = false;
@@ -40,13 +47,15 @@ public class DraggableObject : ObserverSubject
         else
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
+            _collider2D = GetComponent<Collider2D>();
+
             if (_rigidbody2D == null)
             {
                 Debug.LogError("DraggableObject: 2D mode requires a Rigidbody2D component");
                 enabled = false;
             }
 
-            if (GetComponent<Collider2D>() == null)
+            if (_collider2D == null)
             {
                 Debug.LogError("DraggableObject: 2D mode requires a Collider2D component");
                 enabled = false;
@@ -56,6 +65,8 @@ public class DraggableObject : ObserverSubject
 
     private void Update()
     {
+        HandleMouseInput();
+
         if (_isSticky)
             return;
 
@@ -70,6 +81,111 @@ public class DraggableObject : ObserverSubject
                 Handle2DDragging();
             }
         }
+    }
+
+    private void HandleMouseInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (IsMouseOverThisObject())
+            {
+                StartDragging();
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            if (_isDragging)
+            {
+                StopDragging();
+            }
+        }
+    }
+
+    private bool IsMouseOverThisObject()
+    {
+        if (_is3D)
+        {
+            Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit[] hits = Physics.RaycastAll(ray, Mathf.Infinity, draggableLayerMask);
+
+            // Check if this object is the closest draggable object
+            float closestDistance = Mathf.Infinity;
+            GameObject closestDraggable = null;
+
+            foreach (RaycastHit hit in hits)
+            {
+                if (hit.collider.GetComponent<DraggableObject>() != null && hit.distance < closestDistance)
+                {
+                    closestDistance = hit.distance;
+                    closestDraggable = hit.collider.gameObject;
+                }
+            }
+
+            return closestDraggable == gameObject;
+        }
+        else
+        {
+            Vector2 mouseWorldPos = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D[] hits = Physics2D.RaycastAll(mouseWorldPos, Vector2.zero, 0f, draggableLayerMask);
+
+            // Check if this object is the topmost draggable object (highest sorting order or closest to camera)
+            float closestZ = Mathf.Infinity;
+            GameObject closestDraggable = null;
+
+            foreach (RaycastHit2D hit in hits)
+            {
+                if (hit.collider.GetComponent<DraggableObject>() != null)
+                {
+                    float zPos = hit.collider.transform.position.z;
+                    if (zPos < closestZ) // In 2D, smaller Z values are closer to camera
+                    {
+                        closestZ = zPos;
+                        closestDraggable = hit.collider.gameObject;
+                    }
+                }
+            }
+
+            return closestDraggable == gameObject;
+        }
+    }
+
+    private void StartDragging()
+    {
+        if (_is3D)
+        {
+            if (_rigidbody3D)
+            {
+                _rigidbody3D.velocity = Vector3.zero;
+                _isDragging = true;
+                Notify(onDragEvent);
+
+                // Create a plane that is perpendicular to the camera view
+                Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+                _dragPlane = new Plane(_mainCamera.transform.forward, transform.position);
+
+                // Calculate the offset from the hit point to the object's position
+                if (_dragPlane.Raycast(ray, out float distance))
+                {
+                    Vector3 hitPoint = ray.GetPoint(distance);
+                    _dragOffset = transform.position - hitPoint;
+                }
+            }
+        }
+        else
+        {
+            if (_rigidbody2D)
+            {
+                _rigidbody2D.velocity = Vector2.zero;
+                _isDragging = true;
+                Notify(onDragEvent);
+            }
+        }
+    }
+
+    private void StopDragging()
+    {
+        _isDragging = false;
+        Notify(onDropEvent);
     }
 
     private void Handle2DDragging()
@@ -114,45 +230,6 @@ public class DraggableObject : ObserverSubject
             if (_isSticky) return;
             _isDragging = false;
         }
-    }
-
-    private void OnMouseDown()
-    {
-        if (_is3D)
-        {
-            if (_rigidbody3D)
-            {
-                _rigidbody3D.velocity = Vector3.zero;
-                _isDragging = true;
-                Notify(onDragEvent);
-
-                // Create a plane that is perpendicular to the camera view
-                Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
-                _dragPlane = new Plane(_mainCamera.transform.forward, transform.position);
-
-                // Calculate the offset from the hit point to the object's position
-                if (_dragPlane.Raycast(ray, out float distance))
-                {
-                    Vector3 hitPoint = ray.GetPoint(distance);
-                    _dragOffset = transform.position - hitPoint;
-                }
-            }
-        }
-        else
-        {
-            if (_rigidbody2D)
-            {
-                _rigidbody2D.velocity = Vector2.zero;
-                _isDragging = true;
-                Notify(onDragEvent);
-            }
-        }
-    }
-
-    private void OnMouseUp()
-    {
-        _isDragging = false;
-        Notify(onDropEvent);
     }
 
     public void OnNotify(GameEventData gameEventData)
--- Assets/Scripts/common/scripts/SimpleInteraction.cs
+++ Assets/Scripts/common/scripts/SimpleInteraction.cs
@@ -15,33 +15,131 @@ namespace common.scripts
         None
     }
 
-
     [RequireComponent(typeof(SpriteRenderer))]
     public class SimpleInteraction : ObserverSubject
     {
         [SerializeField] private InteractableObjectType objectType;
         [SerializeField] private InteractionType interactionType;
 
-        [FormerlySerializedAs("_disableOnClick")] [SerializeField]
+        [FormerlySerializedAs("_disableOnClick")]
+        [SerializeField]
         private bool disableGameObjectOnClick;
 
         [SerializeField] public bool isEnabled = true;
+        [SerializeField] private LayerMask interactableLayerMask = -1; // Which layers can be interacted with
+        [SerializeField] private bool _is3D = false; // Set to true if this is a 3D object
+
         private SpriteRenderer _spriteRenderer;
+        private Camera _mainCamera;
+        private Collider _collider3D;
+        private Collider2D _collider2D;
 
         private void Start()
         {
             _spriteRenderer = GetComponent<SpriteRenderer>();
+            _mainCamera = Camera.main;
+
+            if (_is3D)
+            {
+                _collider3D = GetComponent<Collider>();
+                if (_collider3D == null)
+                {
+                    Debug.LogError("SimpleInteraction: 3D mode requires a Collider component");
+                    enabled = false;
+                }
+            }
+            else
+            {
+                _collider2D = GetComponent<Collider2D>();
+                if (_collider2D == null)
+                {
+                    Debug.LogError("SimpleInteraction: 2D mode requires a Collider2D component");
+                    enabled = false;
+                }
+            }
         }
 
-        private void OnMouseDown()
+        private void Update()
+        {
+            HandleMouseInput();
+        }
+
+        private void HandleMouseInput()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (IsMouseOverThisObject())
+                {
+                    HandleMouseDown();
+                }
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                if (IsMouseOverThisObject())
+                {
+                    HandleMouseUp();
+                }
+            }
+        }
+
+        private bool IsMouseOverThisObject()
+        {
+            if (!isEnabled) return false;
+
+            if (_is3D)
+            {
+                Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+                RaycastHit[] hits = Physics.RaycastAll(ray, Mathf.Infinity, interactableLayerMask);
+
+                // Check if this object is the closest interactable object
+                float closestDistance = Mathf.Infinity;
+                GameObject closestInteractable = null;
+
+                foreach (RaycastHit hit in hits)
+                {
+                    if (hit.collider.GetComponent<SimpleInteraction>() != null && hit.distance < closestDistance)
+                    {
+                        closestDistance = hit.distance;
+                        closestInteractable = hit.collider.gameObject;
+                    }
+                }
+
+                return closestInteractable == gameObject;
+            }
+            else
+            {
+                Vector2 mouseWorldPos = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+                RaycastHit2D[] hits = Physics2D.RaycastAll(mouseWorldPos, Vector2.zero, 0f, interactableLayerMask);
+
+                // Check if this object is the topmost interactable object (closest to camera)
+                float closestZ = Mathf.Infinity;
+                GameObject closestInteractable = null;
+
+                foreach (RaycastHit2D hit in hits)
+                {
+                    if (hit.collider.GetComponent<SimpleInteraction>() != null)
+                    {
+                        float zPos = hit.collider.transform.position.z;
+                        if (zPos < closestZ) // In 2D, smaller Z values are closer to camera
+                        {
+                            closestZ = zPos;
+                            closestInteractable = hit.collider.gameObject;
+                        }
+                    }
+                }
+
+                return closestInteractable == gameObject;
+            }
+        }
+
+        private void HandleMouseDown()
         {
             if (!isEnabled) return;
-            // FIXME: This does not work for the flashlight for some reason
-            print("Mouse down on" + gameObject.name);
+            print("Mouse down on " + gameObject.name);
             Notify(GameEvents.ClickOnItem, gameObject.name);
         }
 
-        private void OnMouseUp()
+        private void HandleMouseUp()
         {
             if (!isEnabled) return;
 
