4863e50bf5fb49e3b45043b0e69e33302a672841
Eliran Shemesh <piro27@gmail.com>
2025-07-04 15:55:13 +0300

    CSS-44: Kill sequence

--- Assets/Scripts/Elevator/scripts/SelectableKillableCharacter.cs
+++ Assets/Scripts/Elevator/scripts/SelectableKillableCharacter.cs
@@ -1,16 +1,51 @@
+using System.Collections;
+using common.scripts;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class SelectableKillableCharacter : MonoBehaviour
+namespace Elevator.scripts
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [RequireComponent(typeof(Image))]
+    [RequireComponent(typeof(UICompoundMover))]
+    public class SelectableKillableCharacter : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private Image bloodyScreen;
+        [SerializeField] private Sprite deadImage;
+        [SerializeField] private UICompoundMover uiCompoundMover;
+        private Image _image;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Start()
+        {
+            _image = GetComponent<Image>();
+            uiCompoundMover = GetComponent<UICompoundMover>();
+        }
+
+        public void OnNotify()
+        {
+            bloodyScreen.gameObject.SetActive(true);
+            bloodyScreen.GetComponent<TransitionController>().FadeInOut(3);
+            Invoke(nameof(SwitchSprite), 1);
+            Invoke(nameof(StartTransition), 5);
+        }
+
+        private void SwitchSprite()
+        {
+            _image.sprite = deadImage;
+        }
+
+        private void StartTransition()
+        {
+            uiCompoundMover.enabled = false;
+            StartCoroutine(TransitionSpriteDown());
+        }
+
+        private IEnumerator TransitionSpriteDown()
+        {
+            while (transform.position.y > -Screen.height)
+            {
+                transform.Translate(0, -5 * Time.deltaTime, 0);
+                yield return new WaitForEndOfFrame();
+            }
+        }
     }
 }
--- Assets/Scripts/common/scripts/TransitionController.cs
+++ Assets/Scripts/common/scripts/TransitionController.cs
@@ -7,9 +7,15 @@ namespace common.scripts
     [RequireComponent(typeof(Image))]
     public class TransitionController : MonoBehaviour
     {
+        [SerializeField] private Color targetColor = Color.black;
         private Image _overlayImage;
 
         private void Start()
+        {
+            _overlayImage = GetComponent<Image>();
+        }
+
+        private void OnEnable()
         {
             _overlayImage = GetComponent<Image>();
         }
@@ -26,23 +32,27 @@ namespace common.scripts
             StartCoroutine(Fade(false));
         }
 
-        public void FadeInOut()
+        public void FadeInOut(float duration = 0.3f)
         {
             StopAllCoroutines();
             StartCoroutine(Fade(true));
-            Invoke(nameof(FadeOut), 0.3f);
+            Invoke(nameof(FadeOut), duration);
         }
 
-        public void FadeOutIn()
+        public void FadeOutIn(float duration = 0.3f)
         {
             StopAllCoroutines();
             StartCoroutine(Fade(false));
-            Invoke(nameof(FadeIn), 0.3f);
+            Invoke(nameof(FadeIn), duration);
         }
 
         private IEnumerator Fade(bool fadeIn)
         {
-            // var fadeIn = _overlayImage.color.a == 0;
+            if (!_overlayImage)
+            {
+                print("No overlay image");
+                yield break;
+            }
 
             var startAlpha = fadeIn ? 0f : 1f;
             var endAlpha = fadeIn ? 1f : 0f;
@@ -53,12 +63,13 @@ namespace common.scripts
             {
                 elapsedTime += Time.deltaTime;
                 var alpha = Mathf.Lerp(startAlpha, endAlpha, elapsedTime / transitionDuration);
-                _overlayImage.color = new Color(0f, 0f, 0f, alpha);
+
+                _overlayImage.color = new Color(targetColor.r, targetColor.g, targetColor.b, alpha);
                 yield return null;
             }
 
             // Ensure the final alpha is set exactly
-            _overlayImage.color = new Color(0f, 0f, 0f, endAlpha);
+            _overlayImage.color = new Color(targetColor.r, targetColor.g, targetColor.b, endAlpha);
 
             yield return new WaitForEndOfFrame();
         }
