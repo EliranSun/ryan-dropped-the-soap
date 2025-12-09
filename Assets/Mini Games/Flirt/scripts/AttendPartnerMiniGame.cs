c960a759fbcca40e10655392b0dc9ad09a4d48a8
Eliran Shemesh <piro27@gmail.com>
2025-12-09 13:50:21 +0200

    CSS-123: Zeke avatar assets + audio for Attend Partner mini-game
    Added new Zeke avatar images and meta files under Assets/Images/Avatars/Zeke-assets. Introduced audio clips for the Attend Partner mini-game featuring Charlotte, with corresponding meta files. Updated AttendPartnerMiniGame.cs to support partner choice audio and improved button text assignment. Minor adjustments made to the Zeke scene and related scriptable objects to support new assets and features.

--- Assets/Mini Games/Flirt/scripts/AttendPartnerMiniGame.cs
+++ Assets/Mini Games/Flirt/scripts/AttendPartnerMiniGame.cs
@@ -4,6 +4,27 @@ using UnityEngine;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
+/*
+ * Let's go on a date - NO | OK
+ * Give me a kiss - AVOID | OK
+ * Give me a hug - AVOID | OK
+ * Should we try for a kid? - AVOID | OK
+ * Let's have sex  - AVOID | OK
+ * Do you even love me anymore? - NO | YES
+ * We don't make love anymore... - SO | OK
+ * Are you cheating on me? - NO | YES
+ * What do you want for dinner? DUNNO | DONT CARE
+ * Can you take out the trash? AVOID | OK
+ * What should we do for my birthday? - DUNNO | DONT CARE
+ * It's our anniversary tomorrow... - DUNNO | DONT CARE
+ * Let's do something nice - AVOID | OK
+ * Do I look nice in this dress? DUNNO | DONT CARE
+ * You never say nice things to me - SO | OK
+ * What happened to us? AVOID | DUNNO
+ * Hold my hand - AVOID | OK
+ * Can you hold me? - AVOID | OK
+ * I am depressed - SO | OK
+ */
 namespace Mini_Games.Flirt.scripts
 {
     [Serializable]
@@ -18,6 +39,7 @@ namespace Mini_Games.Flirt.scripts
     {
         public Sprite initPartnerExpression;
         public string choiceText;
+        public AudioClip choiceAudio;
         public PlayerResponse[] playerResponses;
     }
 
@@ -34,7 +56,27 @@ namespace Mini_Games.Flirt.scripts
         private void Start()
         {
             partnerBodyImage.sprite = partnerBody;
-            partnerRequest.text = partnerChoices[Random.Range(0, partnerChoices.Length)].choiceText;
+            if (partnerChoices == null || partnerChoices.Length == 0) return;
+
+            var choice = partnerChoices[Random.Range(0, partnerChoices.Length)];
+
+            partnerRequest.text = choice.choiceText;
+            if (choice.initPartnerExpression != null)
+                partnerExpressionImage.sprite = choice.initPartnerExpression;
+
+            if (choice.playerResponses == null || choice.playerResponses.Length == 0) return;
+            SetButtonText(choiceA, choice.playerResponses[0].response);
+
+            if (choice.playerResponses.Length > 1)
+                SetButtonText(choiceB, choice.playerResponses[1].response);
+        }
+
+        private static void SetButtonText(Button button, string text)
+        {
+            if (!button) return;
+
+            var label = button.GetComponentInChildren<TextMeshProUGUI>();
+            if (label != null) label.text = text;
         }
     }
 }
